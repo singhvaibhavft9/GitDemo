67364cd0ab0c4fce077b29bde3c75c03b7b3c893
Vaibhav <singhvaibhavft9@gmail.com>
2021-07-07 14:47:17 +0530

    first commit

---
+++ src/test/java/quickacadamy/MavenJava/RESTAPITest.java
@@ -0,0 +1,18 @@
+package quickacadamy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+
+}
